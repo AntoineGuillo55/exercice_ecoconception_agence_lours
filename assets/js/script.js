12bef7511f0398b8480b9d6c05c3587b916ed9fd
Antoine Guillo <contact@antoineguillo.fr>
2026-07-16 16:15:48 +0200

    déparation du css et du js de la page html, ajout d'un fichier htaccess pour la gestion du cache, modification des balises html pour remplacer certaines div par des balises sémantiques

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    window.addEventListener('scroll', function () {
+        let headerDiv = document.querySelector('.div-header-outer-wrap');
+        if (window.scrollTop() > 50) {
+            headerDiv.style.boxShadow = '0 2px 20px rgba(0,0,0,0.15)';
+
+        } else {
+            headerDiv.style.boxShadow = '0 2px 10px rgba(0,0,0,0.08)';
+        }
+    });
+});
