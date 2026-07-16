bb7c9dd4a3e64959641c470be93262bcf25bff72
Antoine Guillo <contact@antoineguillo.fr>
2026-07-16 19:40:52 +0200

    correction script js et suppression de 2 balises fermantes orphelines

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', function () {
         let headerDiv = document.querySelector('.div-header-outer-wrap');
-        if (window.scrollTop() > 50) {
+        if (window.scrollY > 50) {
             headerDiv.style.boxShadow = '0 2px 20px rgba(0,0,0,0.15)';
 
         } else {
